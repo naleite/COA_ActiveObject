d60c4849fcd1b27605967574117525f3430f5a34
josian <marinier.josian@yahoo.fr>
2015-01-21 08:52:15 +0100

    maj afficheur

--- src/main/java/async/afficheur/Afficheur.java
+++ src/main/java/async/afficheur/Afficheur.java
@@ -3,19 +3,23 @@ package async.afficheur;
 import async.ObserverdeCapteur;
 import async.canal.Canal;
 import async.capteur.Capteur;
+import javafx.scene.control.Label;
 
 /**
  * Created by naleite on 15/1/7.
  */
 public class Afficheur implements ObserverdeCapteur {
     private Canal canal;
+    private Label label;
 
-    public Afficheur(Canal c){
+    public Afficheur(Canal c, Label label){
+        this.label = label;
         this.canal=c;
     }
+
     @Override
     public void update(Capteur subject) {
-
-        this.canal.getValue();
+        int v = this.canal.getValue();
+        label.setText(Integer.toString(v));
     }
 }
